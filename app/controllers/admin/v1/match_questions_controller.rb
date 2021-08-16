2af082c35301ff0e0ec899919f5d2d982bff818a
Giancarlo Guzman Oki <giancarloguzo@gmail.com>
2021-08-16 00:36:13 -0500

    Add worker to update all picks

--- app/controllers/admin/v1/match_questions_controller.rb
+++ app/controllers/admin/v1/match_questions_controller.rb
@@ -5,6 +5,7 @@ module Admin
         match_question = MatchQuestion.find_by(id: params[:match_question_id])
         if match_question
           if match_question.update(winner_id: params[:winner])
+            PicksUpdateWorker.perform_async(match_question.id)
             render json: { message: 'Question updated' }
           else
             render json: { message: 'There was an error' }
---
+++ app/workers/picks_update_worker.rb
@@ -0,0 +1,14 @@
+class PicksUpdateWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :default
+
+  def perform(match_question_id)
+    match_question = MatchQuestion.find(match_question_id)
+    if match_question
+      winner_picks = Pick.where(match_question_id: match_question_id, team_id: match_question.winner_id)
+      winner_picks.update_all(result: true)
+      loser_picks = Pick.where(match_question_id: match_question_id).where.not(team_id: match_question.winner_id)
+      loser_picks.update_all(result: false)
+    end
+  end
+end
